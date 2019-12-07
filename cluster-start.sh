f38f8a8a5b2d7006f92ca66df335af14f08556d6
Shravan Shetty <shsh9888@colorado.edu>
2019-12-07 15:21:12 -0700

    Added cluster creation and wait for Cassandra

--- cluster-start.sh
+++ cluster-start.sh
@@ -3,6 +3,10 @@
 #cqlsh -e "SOURCE 'schema.cql'"
 #cqlsh -e "copy iot.buildinginfo from 'buildinginfo.csv' with header=true"
 #cqlsh -e "copy iot.deviceinfo from 'deviceinfo.csv' with header=true"
+#create a cluster
+gcloud config set compute/zone us-west1-b
+gcloud container clusters create dcproject
+
 
 ##clone all the required repositories
 git clone https://github.com/shsh9888/kafka-on-kubernetes
@@ -17,7 +21,7 @@ cp cassandra-on-kubernetes/*.yaml ./build/
 
 ##running Kafka and Cassandra
 cd build && kubectl create -f .
-
+``
 cd .. && cd flink/flink-container/kubernetes/
 
 kubectl create -f job-cluster-service.yaml
@@ -29,6 +33,9 @@ FLINK_IMAGE_NAME=gcr.io/lab7-258103/fuck:1.0 FLINK_JOB_PARALLELISM=1 envsubst < 
 
 cd ../../../
 
+echo "Waiting for Cassandra, Wating for a minute.If you see an error here you might have to increase sleep"
+sleep 1m
+
 
 ###Adding data to Cassandra
 find_cassandra_pods="kubectl get pods -l name=cassandra"
@@ -39,6 +46,8 @@ first_running_seed=$($find_cassandra_pods --no-headers | \
     head -1 | \
     awk '{print $1}')
 
+echo "Pod found for Cassandra : $first_running_seed"
+
 kubectl cp schema.cql $first_running_seed:/home/
 kubectl cp deviceinfo.csv $first_running_seed:/home/
 kubectl cp buildinginfo.csv $first_running_seed:/home/
@@ -48,7 +57,6 @@ kubectl cp buildinginfo.csv $first_running_seed:/home/
 kubectl exec -it $first_running_seed -- bash -c "cqlsh -e \"SOURCE '/home/schema.cql'"\"
 kubectl exec -it $first_running_seed -- bash -c "cqlsh -e \"copy iot.buildinginfo from '/home/buildinginfo.csv' with header=true\""
 kubectl exec -it $first_running_seed -- bash -c "cqlsh -e \"copy iot.deviceinfo from '/home/deviceinfo.csv' with header=true\""
-
 
 
 ##running the simulator: data producer  
